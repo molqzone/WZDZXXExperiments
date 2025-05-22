2be156ab41faf7afde4d4e92db2091f833622f03
molqzone <toiperus@sohu.com>
2025-05-22 08:04:32 +0800

    Initial commit: Add JavaPractices directory and text file

---
+++ JavaPractices/Hello.java
@@ -0,0 +1,9 @@
+/*
+ * This is a simple Java program that contains an empty class named Hello.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        // This is the main method, which is the entry point of the program.
+        System.out.println("Hello, World!");
+    }
+}
